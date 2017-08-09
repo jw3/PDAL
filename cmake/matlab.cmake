194246b200228776d6ef439d5351581712ded31c
John Wass <wassj@ctc.com>
2017-08-08 22:34:01 -0400

    add current matlab versions

--- cmake/matlab.cmake
+++ cmake/matlab.cmake
@@ -1,6 +1,11 @@
 #
 # Matlab
 #
+set(MATLAB_ADDITIONAL_VERSIONS
+        "R2017a=9.2"
+        "R2016b=9.1"
+        "R2016a=9.0")
+
 find_package(Matlab COMPONENTS ENG_LIBRARY MX_LIBRARY MEX_LIBRARY REQUIRED)
 set_package_properties(Matlab PROPERTIES TYPE REQUIRED)
 if(MATLAB_FOUND)
